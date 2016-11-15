c55574c50e1abf880110862a1a316b73434a55a2
blond <andrewblond@yandex.com>
2016-11-15 12:52:31 +0300

    docs(jsdoc): add jsdoc

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 
 const path = require('path');
 
+/**
+ * Returns basename without extension.
+ *
+ * @param {String} filename — the path to file or directory.
+ *
+ * @returns {String}
+ */
 module.exports = (filename) => path.parse(filename).name;
